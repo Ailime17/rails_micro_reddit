459c1d6b162e832a23f12991aebc21c1afa03983
Emilia <emilia.skrzypczak.es@gmail.com>
2023-03-28 11:29:43 +0200

    create User-Post association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :password, presence: true, length: { in: 5..20 }
 end
