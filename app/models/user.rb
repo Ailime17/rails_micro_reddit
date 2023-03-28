b3ee229d2d834d2ac3a172d558f11fab3b00f03a
Emilia <emilia.skrzypczak.es@gmail.com>
2023-03-28 11:58:38 +0200

    add User-Comment & Post-Comment associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
+  has_many :comments
 
   validates :username, presence: true, length: { in: 2..20 }, uniqueness: true
   validates :password, presence: true, length: { in: 5..20 }
